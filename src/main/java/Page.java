5c0879ca7943b239948491eae91e5c8898d2ec33
horvathjanos0712 <horvathjanos0712@outlook.hu>
2022-06-22 21:51:04 +0200

    g

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Page {
@@ -13,6 +14,11 @@ public class Page {
 
     private final String urlBasic = "http://demo.seleniumeasy.com/basic-first-form-demo.html";
     private final By popupX = By.xpath("//*[@id=\"at-cv-lightbox-close\"]");
+    private final By a = By.xpath("//*[@id=\"sum1\"]");
+    private final By b = By.xpath("//*[@id=\"sum2\"]");
+    private final By button = By.xpath("//*[@id=\"gettotal\"]/button");
+    private final By result = By.xpath("//*[@id=\"displayvalue\"]");
+
 
     public void navigateToBasic() {
         driver.get(urlBasic);
@@ -20,6 +26,22 @@ public class Page {
     public void clickX() {
         driver.findElement(popupX).click();
     }
+    public void fillA() {
+        driver.findElement(a).sendKeys("5");
+    }
+    public void fillB() {
+        driver.findElement(b).sendKeys("5");
+    }
+    public void clickButton() {
+        driver.findElement(button).click();
+    }
+    public String getResult() {
+        return driver.findElement(result).getText();
+
+    }
+
+
+
 }
 
 
--- src/test/java/InputFieldTest.java
+++ src/test/java/InputFieldTest.java
@@ -1,12 +1,27 @@
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
 
 public class InputFieldTest extends BaseTest {
 
     @Test
-    public void TestSumTwoNumbers() {
+    public void TestSumTwoNumbers() throws InterruptedException {
         Page page = new Page(driver, wait);
         page.navigateToBasic();
         page.clickX();
+        page.fillA();
+        page.fillB();
+        page.clickButton();
+        String actual = page.getResult();
+        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Thread.sleep(2000);
+        Assertions.assertEquals("10", actual);
+        Thread.sleep(2000);
+        
     }
 
 
